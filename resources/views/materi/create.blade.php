95390e560789c1ff6e5d7c834b3c7b3192d9f456
com_bot <aldiruli666@gmail.com>
2023-07-25 18:15:01 +0700

    judul eanu

--- resources/views/materi/create.blade.php
+++ resources/views/materi/create.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.appAdmin')
-@section('title', 'Siswa')
+@section('title', 'Materi')
 @section('siswaAdmin')
 
 <!-- Begin Page Content -->
@@ -9,7 +9,7 @@
         <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
             <ol class="breadcrumb">
               <li class="breadcrumb-item"><a href="/home">{{ __("Dashboard") }}</a></li>
-              <li class="breadcrumb-item active" aria-current="page">{{ __("Siswa") }}</li>
+              <li class="breadcrumb-item active" aria-current="page">{{ __("Materi") }}</li>
             </ol>
           </nav>
     </div>
